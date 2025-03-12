ec92e9cfc0c7a1a0687e469f28ccc0e468a9f4c8
nediazla <nediazla@gmail.com>
2025-03-12 02:29:23 +0100

    16

--- Diario/ui.py
+++
@@ -1,9 +0,0 @@
-import re
-from turtledemo.penrose import start
-
-regex = r'\d{2}'
-
-txt = 'Apple and banana are fruits. An old cliche 25 says an apple a day a doctor way has 2020 been replaced by a banana a day keeps the doctor far far away.'
-
-match = re.findall(regex, txt)
-print(match)
